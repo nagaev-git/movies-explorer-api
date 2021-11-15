0c6a9242e7c555fb265092f286061e36083e3435
Aleksandr Nagaev <nagaevvvvv@yandex.ru>
2021-11-15 18:05:33 +0300

    feat: добавить модель user и movie

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minlength: [2, "Минимальная длина поля 'name' - 2 символа."],
+      maxlength: [30, "Максимальная длина поля 'name' - 30 символов."],
+    },
+    email: {
+      type: String,
+      unique: [true, "Пользователь с таким 'email' уже существует."],
+      required: [true, "Поле 'email' должно быть заполнено."],
+      validate: {
+        validator: (v) => isEmail(v),
+        message: "Неправильный формат почты.",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Поле 'password' должно быть заполнено."],
+      select: false,
+    },
+  },
+  { versionKey: false },
+);
+
+module.exports = mongoose.model("user", userSchema);
